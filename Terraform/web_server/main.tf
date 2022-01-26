a1106ef5c9647e4feff7c3f400f4dd07991873e4
EC2 Default User <ec2-user@ip-172-31-15-211.us-east-2.compute.internal>
2022-01-26 03:54:05 +0000

    Added variables to single server tf

--- Terraform/web_server/main.tf
+++ Terraform/web_server/main.tf
@@ -1,4 +1,4 @@
-resource "aws_launch_configuration" "example" {
+resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-01963b92a9a1e6020",]
@@ -6,32 +6,22 @@ resource "aws_launch_configuration" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   
   tags = {
     Name = "terraform-example"
   }
-
-  lifecycle {
-    create_before_detroy = true
-  }
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.ansible.id
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.ansible.ids
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key = "Name"
-    value = "terraform-asg-example"
-    propagate_at_launch = true
-  }
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
+

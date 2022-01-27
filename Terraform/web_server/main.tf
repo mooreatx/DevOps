43c6ae716d2acc60a276c0f1391a295efa110466
EC2 Default User <ec2-user@ip-172-31-15-211.us-east-2.compute.internal>
2022-01-27 03:29:46 +0000

    Created ASG

--- Terraform/web_server/main.tf
+++ Terraform/web_server/main.tf
@@ -1,16 +1,42 @@
-resource "aws_instance" "example" {
-  ami = "ami-0c55b159cbfafe1f0"
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-01963b92a9a1e6020",]
+  security_groups = ["sg-01963b92a9a1e6020",]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  
-  tags = {
-    Name = "terraform-example"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -19,9 +45,3 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
-
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
-
